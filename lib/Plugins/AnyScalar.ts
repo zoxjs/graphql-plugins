ccc9c02c900860d2bac0a3ab0b3f0951731d299e
Zoran Ravic <contact@zoranravic.com>
2019-08-06 00:24:28 +0200

    Any and Object scalars can be used to allow arbitrary inputs and outputs

---
+++ lib/Plugins/AnyScalar.ts
@@ -0,0 +1,50 @@
+import {IScalar, Scalar} from "../PluginManagers/ScalarPluginManager";
+import {Kind, ObjectValueNode, ValueNode} from "graphql";
+
+export const AnyDef = `
+scalar Any
+`;
+
+@Scalar('Any', AnyDef)
+export class AnyScalar implements IScalar<any, any>
+{
+    public serialize = identity;
+    public parseValue = identity;
+    public parseLiteral = parseLiteral;
+}
+
+function identity(value)
+{
+    return value;
+}
+
+function parseLiteral(ast: ValueNode, variables?: { [p:string]: any }): any
+{
+    switch (ast.kind)
+    {
+        case Kind.STRING:
+        case Kind.BOOLEAN:
+            return ast.value;
+        case Kind.INT:
+        case Kind.FLOAT:
+            return parseFloat(ast.value);
+        case Kind.OBJECT:
+            return parseObject(ast, variables);
+        case Kind.LIST:
+            return ast.values.map(n => parseLiteral(n, variables));
+        case Kind.NULL:
+            return null;
+        case Kind.VARIABLE:
+            return variables ? variables[ast.name.value] : undefined;
+    }
+}
+
+export function parseObject(ast: ObjectValueNode, variables?: { [p:string]: any })
+{
+    const value = {};
+    for (const field of ast.fields)
+    {
+        value[field.name.value] = parseLiteral(field.value, variables);
+    }
+    return value;
+}
---
+++ lib/Plugins/ObjectScalar.ts
@@ -0,0 +1,31 @@
+import {IScalar, Scalar} from "../PluginManagers/ScalarPluginManager";
+import {Kind, ValueNode} from "graphql";
+import {parseObject} from "./AnyScalar";
+
+export const ObjectDef = `
+scalar Object
+`;
+
+@Scalar('Object', ObjectDef)
+export class ObjectScalar implements IScalar<Object, Object>
+{
+    public serialize = ensureObject;
+    public parseValue = ensureObject;
+
+    public parseLiteral(ast: ValueNode, variables?: { [p:string]: any }): Object | void
+    {
+        if (ast.kind === Kind.OBJECT)
+        {
+            return parseObject(ast, variables);
+        }
+    }
+}
+
+function ensureObject(value: Object): Object
+{
+    if (typeof value !== 'object' || value === null || Array.isArray(value))
+    {
+        throw new TypeError(`Object cannot represent non-object value: '${value}'`);
+    }
+    return value;
+}
